62f210702a5814d85b329d2368181cdcab8a5c67
Brandon Cox <brandon.cox.519@gmail.com>
2013-07-03 22:02:11 -0400

    Added new layout for devise based sign in/registration and config.

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,10 @@ end
 
 module Iou
   class Application < Rails::Application
+    
+    config.to_prepare do
+        Devise::SessionsController.layout proc{ |controller| action_name == 'new' ? "devise"   : "application" }
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
